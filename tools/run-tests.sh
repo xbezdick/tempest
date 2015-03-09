60aef39419e5c36d52ba9b47fa66d3f90f249acd
Pavel Sedlák <psedlak@redhat.com>
2015-03-09 16:24:13 +0100

    run-tests: passthrough testr longopts
    Change-Id: Icccc3cab7963fbe30b6cd8d332f3d0c4f6b42144

--- tools/run-tests.sh
+++ tools/run-tests.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
-set -eu
+set -e
 set -o pipefail
 
 usage() {
-    echo "$(basename $0) [--skip-file file] test-regex1 test-regex2 ..."
+    echo "$(basename $0) [--skip-file <file>] [--<any-testr-long-opts>, ...]  [test-regex-to-include ...]"
+    echo ""
     echo "Format of skip file:\n"
     echo "Whitelist is applied first. The blacklist is executed against the set
  of tests returned by the whitelist.
@@ -30,20 +31,49 @@ is implemented as:
     +tempest\.scenario
     -tempest\.api\.volume.*
 "
+    [[ -z "$1" ]] || exit $1;
 }
 
-if [ "$#" == "0" ] || [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
-    usage
-    exit 1
-fi
-
+t2skip_args=()
 skip_file=/dev/null
-if [[ $1 == '--skip-file' ]]; then
+testr_args=("--parallel")
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -h|--help)
+            usage 0
+            ;;
+        # testr args (first with value, then bool flags)
+        --concurrency|--load-list)
+            testr_args=( "${testr_args[@]}" "$1" "$2" ); shift;
+            ;;
+        --concurrency=*|--load-list=*)
+            testr_args=( "${testr_args[@]}" "$1" );
+            ;;
+        --failing|--parallel|--partial|--subunit|--full-results|--until-failure|--analyze-isolation|--isolated)
+            testr_args=( "${testr_args[@]}" "$1" );
+            ;;
+        # tests2skip args
+        --skip-file)
+            if [[ ! -f "$2" ]]; then
+                echo "Specified skip-file '$(readlink -f $2)' does not exists!" >&2
+                exit 1
+            fi
+            skip_file="$2"
+            shift;
+            ;;
+        *)
+            t2skip_args=( "${t2skip_args[@]}" "$1" );
+            ;;
+    esac
     shift
-    skip_file=$1
-    shift
+done
+
+t2skip_args=("$skip_file" "${t2skip_args[@]}");
+
+if [ ! -d .testrepository ]; then
+    testr init
 fi
 
-testr run --parallel --subunit $($(dirname $0)/tests2skip.py $skip_file $@) | tee >( subunit2junitxml --output-to=tempest.xml ) | subunit-trace --no-failure-debug -f
-
+testr run "${testr_args[@]}" --subunit $($(dirname $0)/tests2skip.py "${t2skip_args[@]}") | tee >( subunit2junitxml --output-to=tempest.xml ) | subunit-trace --no-failure-debug -f
 
